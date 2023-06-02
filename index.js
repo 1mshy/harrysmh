ea1a0bebf0c0aa40e05e10b0bc0ab3bb2daf2ad0
1mshy <imshi1@protonmail.com>
2023-06-02 19:26:39 -0400

    bot for harry's server

--- index.js
+++ index.js
@@ -1,7 +1,81 @@
-if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
-  console.error('Your node version is currently', process.versions.node)
-  console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
-  process.exit(1)
+const mineflayer = require('mineflayer')
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder')
+const pvp = require('mineflayer-pvp').plugin
+const Vec3 = require('vec3').Vec3;
+
+const bot = mineflayer.createBot(
+    {
+        host: 'harrys.network',
+        auth: 'microsoft',
+        username: 'Lazy_Kid',
+        version: '1.8.9'
+    }
+)
+const bot2 = mineflayer.createBot(
+    {
+        host: 'harrys.network',
+        auth: 'microsoft',
+        username: 'uwureach',
+        version: '1.8.9'
+    }
+)
+
+async function setupBot(tempbot) {
+    await tempbot.loadPlugin(pathfinder)
+    await tempbot.loadPlugin(pvp)
+    await tempbot.on('kicked', console.log)
+    await tempbot.on('chat', async (username, message) => {
+        if (username === tempbot.username) return
+        if(username == "DISCORD" ) {
+            joinLobby(tempbot)
+        }
+        if(message == "why") {
+            walktoMid(tempbot)
+        }
+        console.log(`${username} said "${message}"`)
+      })
+      await tempbot.on("error", (err) => {
+        console.log(err)
+    })
+    let counter = 0
+    await tempbot.on("physicTick", async () => {
+        if(counter%41==0) {
+            walktoMid(tempbot)
+            counter -= 40
+        }
+        counter+= 1;
+    })
+}
+function setupBots(array1) {
+    array1.forEach(
+        (tempbot) => {
+            console.log(tempbot.username + " is being loaded")
+            setupBot(tempbot)
+        }
+    )
 }
 
-module.exports = require('./lib/loader.js')
+async function walktoMid(tempbot) {
+    var pos = new Vec3(0, 73, 0);
+    tempbot.lookAt(pos)
+    tempbot.setControlState('forward', true)
+    await sleep(1600)
+    tempbot.setControlState('forward', false)
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+async function joinLobby(tempbot) {
+    await sleep(5000)
+    await console.log(tempbot.entity.username + " is joining lobby")
+    await tempbot.chat('/play pit')
+    await console.log("Success!!")
+}
+
+
+let arrayOfBots = [bot, bot2]
+setupBots(arrayOfBots)
---
+++ run.bat
@@ -0,0 +1,4 @@
+call npm install mineflayer
+call npm install --save mineflayer-pvp
+
+call node index.js
